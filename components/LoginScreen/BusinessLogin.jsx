8c374489e6b13eb27e6ee8c41900b026f2f1d2f2
Andres4L <andresmaltamar15@gmail.com>
2023-02-28 14:05:37 -0500

    otra vista

--- components/LoginScreen/BusinessLogin.jsx
+++ components/LoginScreen/BusinessLogin.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable, Image } from "react-native";
+import { StyleSheet, Text, View, Pressable, Image, TextInput, SafeAreaView } from "react-native";
 
 import React from "react";
 import {
@@ -36,9 +36,26 @@ const BusinessLogin = ({ navigation }) => {
             </View>
           </View>
         </View>
-      </View>
-      <View styles={styles.imageBackgroundContainer}>
-        <Text>imageBackgroundContainer</Text>
+        <View style={styles.textBContainer}>
+          <TextInput
+            style={styles.textInput1}
+            placeholder="Usuario"
+          >
+          </TextInput>
+          <TextInput style={styles.textInput2} placeholder="Celular" keyboardType="numeric">
+          </TextInput>
+          <Pressable>
+            <View style={styles.asIngresaButton}>
+              <Text style={{ color: colors.white }}>Ingresar</Text>
+            </View>
+          </Pressable>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.loginBackgroundImages}
+            source={{ uri: images.loginImage }}
+          />
+        </View>
       </View>
     </View>
   );
@@ -105,5 +122,57 @@ const styles = StyleSheet.create({
     marginTop: 20,
     textAlign: "center",
     ...getFontStyles(13, 0.5, 0.9),
+  },
+  loginBackgroundImages: {
+    width: "100%",
+    height: "100%",
+  },
+  textInput1: {
+    fontFamily: "Poppins-Regular",
+    width:233,
+    height: 50,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: colors.white,
+    borderRadius: 5,
+  },
+  textInput2: {
+    fontFamily: "Poppins-Regular",
+    width:233,
+    height: 50,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: colors.white,
+    borderRadius: 5,
+  },
+
+  asIngresaButton: {
+    backgroundColor: colors.mainBlue,
+    fontFamily: "Poppins-Regular",
+    height: 50,
+    width: 233,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    borderWidth : 2,
+    borderColor: colors.white
+
+  },
+  textBContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    width: "100%",
+    marginTop: 30,
+  },
+  imageContainer: {
+    height: "80%",
+    width: "100%",
   },
 });
